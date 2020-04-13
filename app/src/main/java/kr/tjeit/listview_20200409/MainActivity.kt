ec2b49bf6b07aad58aa456564d576d03efe6dd8a
cho881020 <kj_cho@idealidea.co.kr>
2020-04-13 20:20:36 +0900

    어댑터 / 리스트뷰 연결

--- app/src/main/java/kr/tjeit/listview_20200409/MainActivity.kt
+++ app/src/main/java/kr/tjeit/listview_20200409/MainActivity.kt
@@ -2,10 +2,13 @@ package kr.tjeit.listview_20200409
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kr.tjeit.listview_20200409.adapters.RoomAdapter
 import kr.tjeit.listview_20200409.datas.Room
 
 class MainActivity : AppCompatActivity() {
 
+    var mRoomAdapter:RoomAdapter? = null
     val roomList = ArrayList<Room>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,5 +22,11 @@ class MainActivity : AppCompatActivity() {
         roomList.add(Room(4000, "서울시 성북구", 8, "단독주택입니다."))
         roomList.add(Room(9000, "경기도 남양주시", 12, "아파트입니다."))
         roomList.add(Room(11000, "서울시 광진구", -1, "연립주택입니다."))
+
+
+        mRoomAdapter = RoomAdapter(this, R.layout.room_list_item, roomList)
+        roomListView.adapter = mRoomAdapter
+
+
     }
 }
