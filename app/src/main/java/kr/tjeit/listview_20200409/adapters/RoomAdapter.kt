1cb3f0db8b76634d23b6f2d715f2b8c98869c321
cho881020 <kj_cho@idealidea.co.kr>
2020-04-13 20:14:13 +0900

    리스트뷰 필요 데이터 작성

--- app/src/main/java/kr/tjeit/listview_20200409/adapters/RoomAdapter.kt
+++ app/src/main/java/kr/tjeit/listview_20200409/adapters/RoomAdapter.kt
@@ -8,7 +8,7 @@ import android.widget.ArrayAdapter
 import kr.tjeit.listview_20200409.R
 import kr.tjeit.listview_20200409.datas.Room
 
-class RoomAdapter(context:Context, resId:Int, list:ArrayList<Room>) : ArrayAdapter<Room>(context, resId, list)  {
+class RoomAdapter(context: Context, resId:Int, list: ArrayList<Room>) : ArrayAdapter<Room>(context, resId, list) {
 
     val mContext = context
     val mList = list
@@ -22,8 +22,11 @@ class RoomAdapter(context:Context, resId:Int, list:ArrayList<Room>) : ArrayAdapt
 
         val row = tempRow!!
 
+//        row변수를 활용해서 각줄에 맞는 데이터 가공.
+
         return row
 
     }
+
 
 }
